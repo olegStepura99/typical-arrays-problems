4f391626bd54485954bf9734ab52337fabe4b392
olegStepura99 <79783293+olegStepura99@users.noreply.github.com>
2021-03-11 12:21:03 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-
 exports.min = function min (array) {
-  return 0;
+  if(array==undefined || array.length == 0 ){
+    return 0
+  }
+  let minNumber = array[1]; 
+  for(let i=0;i<array.length;i++){
+    if(array[i]<minNumber){
+      minNumber=array[i];
+    }
+  }
+  return minNumber;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array==undefined || array.length == 0 ){
+    return 0
+  }
+  let maxNumber = 0; 
+  for(let i=0;i<array.length;i++){
+    if(array[i]>maxNumber){
+      maxNumber=array[i];
+    }
+  }
+  return maxNumber;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array===undefined || array.length == 0 ){
+    return 0
+  }
+
+  let summ = 0;
+  for(let i=0; i<array.length;i++){
+    summ+=parseInt(array[i]);
+  }
+  return (summ/array.length);
 }
